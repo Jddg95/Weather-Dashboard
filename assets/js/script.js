5d3e8ef6b84b9a15e89bae2e9ba4d6898c7900ff
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-03-28 16:40:39 -0500

    started fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-const searchedEl = document.getElementById('searched-cities');
+const searchButton = document.getElementById('searchButton');
+const searchHistory = document.getElementById('searchHistory');
+const currentWeather = document.getElementById('currentWeather');
+const weatherForecast = document.getElementById('weatherForeast');
 
-function init() {
-    const searchedLocations = JSON.
+
+function getAPI() {
+    const weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={4429f2f9ac731a6d8a33fa9befeff361}';
+
+    fetch (weatherAPI)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
 }
+searchButton.addEventListener('click', getAPI);
