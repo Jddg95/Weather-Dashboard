4e66c962c985931ce3543bdcf20e79adfa7c27b7
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-03-29 16:07:32 -0500

    linked script to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ const searchHistory = document.getElementById('searchHistory');
 const currentWeather = document.getElementById('currentWeather');
 const weatherForecast = document.getElementById('weatherForeast');
 
-
+console.log(searchButton);
 function getAPI() {
-    const weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={4429f2f9ac731a6d8a33fa9befeff361}';
-
+    const weatherAPI = 'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid=4429f2f9ac731a6d8a33fa9befeff361';
+    
     fetch (weatherAPI)
     .then(function (response) {
         return response.json();
